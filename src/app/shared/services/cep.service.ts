6191cac68cf6ee5e0e8e76bb146cb680e0ba998f
cesar henrique <cesar_silk321@hotmail.com>
2022-05-22 10:53:35 -0300

    endpoint

--- src/app/shared/services/cep.service.ts
+++ src/app/shared/services/cep.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CepService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getCep(cep: string): Observable<any> {
+    return this.http.get(`${environment.apiUrl}/${cep}/json/`);
+  }
 }
